af0b6e422d8775384af37f7291e7f935931b5bf6
Fredrik Svärd <fredrik.svard@svenskaspel.se>
2023-05-21 07:14:22 +0200

    first draft 2

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
-
+import git
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello POE!'
+
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    repo = git.Repo('https://github.com/nfredrik/gaupa.git')
+    origin = repo.remotes.origin
+    origin.pull()
+    return 'Updated PythonAnywhere successfully', 200
 
 
 if __name__ == '__main__':
