ab733c6f85a9ec05a469df85bc5d79f03f07cdff
Fredrik Svärd <nfredrik@hotmail.com>
2023-05-21 20:42:54 +0200

    first draft 2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello POE och Freddan!'
+    return 'Hello POE och Freddan igen!'
 
 
 @app.route('/update_server', methods=['POST'])
